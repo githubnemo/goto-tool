cee13d22f6195f2f3ff816af274b8c7144d86e52
nemo <nemo@ikkoku.de>
2011-12-01 23:10:21 +0100

    Add subdir parameter to goto
    It's now possible to add a subdir parameter relative to the
    directory given to `goto`.
    
    Example:
    
    	$ goto-tool add project ~/myproject
    	$ goto project ../subdir/foo
    	...Changes to ~/myproject/../subdir/foo...
    
    Tab completion is added for this parameter, too.

--- goto.bash
+++ goto.bash
@@ -1,9 +1,19 @@
 # To be sourced in your .bashrc
 
 goto() {
+	if [ "$#" -eq 0 ]; then
+		echo "Usage: goto <dirname> [<subpath>]"
+		return 1
+	fi
+
 	DIR=$(goto-tool get $1)
+
 	if [ "$?" -eq 0 ]; then
-		cd $DIR
+		if [ "$#" -gt 1 ]; then
+			cd $DIR/$2
+		else
+			cd $DIR
+		fi
 	fi
 }
 
