e99725a58c8d9e037df4b12d31e41620cde334a8
nemo <nemo@ikkoku.de>
2014-08-27 19:26:33 +0200

    Sort tests a bit

--- simpletest.sh
+++ simpletest.sh
@@ -2,40 +2,53 @@
 
 . `dirname $0`/simpletest_engine.sh
 
-export GOTO_FILE=`tempfile`
+test_overall_workflow() {
+	export GOTO_FILE=`tempfile`
+	must goto-tool list
+	must goto-tool add foo /tmp
 
-must goto-tool list
-must goto-tool add foo /tmp
+	# Should work after adding the entry 'foo'.
+	must goto-tool use foo
+	must goto-tool get foo
 
-# Should work after adding the entry 'foo'.
-must goto-tool use foo
-must goto-tool get foo
+	# Test using several times to mimic normal behaviour.
+	must goto-tool use foo
+	must goto-tool use foo
+	must goto-tool use foo
 
-# Test using several times to mimic normal behaviour.
-must goto-tool use foo
-must goto-tool use foo
-must goto-tool use foo
+	# Delete foo and expect every command that accesses foo to fail.
+	must goto-tool rm foo
+	fail goto-tool get foo
+	fail goto-tool use foo
+	fail goto-tool use foo
 
-# Delete foo and expect every command that accesses foo to fail.
-must goto-tool rm foo
-fail goto-tool get foo
-fail goto-tool use foo
-fail goto-tool use foo
+	# Should fail since there is no 'bar'.
+	fail goto-tool use bar
+	fail goto-tool rm bar
+	fail goto-tool get bar
 
-# Should fail since there is no 'bar'.
-fail goto-tool use bar
-fail goto-tool rm bar
-fail goto-tool get bar
+	# Switch GOTO_FILE to another file, entries should be different.
+	must goto-tool add foo /tmp
+	must goto-tool get foo
+	ALT_GOTO_FILE=`tempfile`
+	GOTO_FILE="$ALT_GOTO_FILE" fail goto-tool get foo
+	GOTO_FILE="$ALT_GOTO_FILE" must goto-tool add foo /tmp
+	GOTO_FILE="$ALT_GOTO_FILE" must goto-tool get foo
+}
 
-# Simply invoking 'clean' should not raise exceptions
-must goto-tool clean
+test_clean() {
+	# Simply invoking 'clean' should not raise exceptions
+	export GOTO_FILE=`tempfile`
+	must goto-tool clean
+	must goto-tool add foo /tmp
+	must goto-tool clean
+	must goto-tool add bar /tmp
+	must goto-tool clean
+	must goto-tool rm bar
+	must goto-tool clean
+}
 
-# Switch GOTO_FILE to another file, entries should be different.
-must goto-tool add foo /tmp
-must goto-tool get foo
-ALT_GOTO_FILE=`tempfile`
-GOTO_FILE="$ALT_GOTO_FILE" fail goto-tool get foo
-GOTO_FILE="$ALT_GOTO_FILE" must goto-tool add foo /tmp
-GOTO_FILE="$ALT_GOTO_FILE" must goto-tool get foo
+test_overall_workflow
+test_clean
 
 printok "Everything went well."
